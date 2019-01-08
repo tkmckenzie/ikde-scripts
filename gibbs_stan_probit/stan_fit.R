e484c7beb82ecf7bd95c49ac074717affaaee612
tmcken <tmcken@sandia.gov>
2019-01-08 08:16:29 -0700

    NA

--- gibbs_stan_probit/stan_fit.R
+++ gibbs_stan_probit/stan_fit.R
@@ -7,14 +7,14 @@ data("prostatic.nodes")
 y = prostatic.nodes$y
 X = cbind(log(prostatic.nodes$X.1), prostatic.nodes$X.3, prostatic.nodes$X.5)
 
-data = list(N = list("int<lower=1>", nrow(X)),
-            k = list("int<lower=1>", ncol(X)),
-            X = list("matrix[N,k]", X),
-            y = list("int[N]", y))
-parameters = list(beta = "vector[k]")
-transformed.parameters = list(z = c("vector[N]", "z = X * beta"))
-model = list(priors = "beta ~ normal(0, 10)",
-             likelihood = "y ~ bernoulli(Phi(z))")
+data = list(N = list(type = "int<lower=1>", dim = 1, value = nrow(X)),
+            k = list(type = "int<lower=1>", dim = 1, value = ncol(X)),
+            X = list(type = "matrix", dim = "[N, k]", value = X),
+            y = list(type = "int", dim = "[N]", value = y))
+parameters = list(beta = list(type = "vector", dim = "k"))
+transformed.parameters = list(z = list(type = "vector", dim = "N", expression = "z = X * beta;"))
+model = list(priors = "beta ~ normal(0, 10);",
+             likelihood = "y ~ bernoulli(Phi(z));")
 
 ikde.model = define.model(data, parameters, model, transformed.parameters = transformed.parameters)
 ikde.model = build.model(ikde.model)
